5ee8bbc6c059f7d6a7eba8edf55f1225906e9fb5
Vineet Kumar Gupta <vk779756@gmail.com>
2020-10-01 13:13:28 +0530

    Update app.py
    Added option for deployment and development server ( deployment on heroku or any platform)

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ Main server file returning the news dictionary from inshorts file as JSON Object
 from flask import Flask, request, jsonify
 from inshorts import getNews
 from flask_cors import CORS
-
+env= 1
+# If deploying on cloud server such as heroku use env =0  else use development server ie env = 1
 app = Flask(__name__)
 CORS(app)
 
@@ -19,5 +20,10 @@ def news():
     elif request.method == 'GET':
         return jsonify(getNews(request.args.get('category')))
 
-app.run(debug=True)
+    
+if (env == 0):
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+else:
+    app.run(debug=True)
         
